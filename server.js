e4c4c5980cbba53a3bc04ecc392e408649ddec9b
0TheDragon0 <arianajavadi3@gmail.com>
2021-07-03 11:02:08 +0200

    Added QA enviroment to allowed origins

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.use(passport.session());
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Origin', ['http://localhost:4200', 'https://ari-edu.firebaseapp.com']);
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
