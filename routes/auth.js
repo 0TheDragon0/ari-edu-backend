9ecb73281755e06a93d01575cd7414bbcac1b5a5
0TheDragon0 <arianajavadi3@gmail.com>
2021-07-31 11:46:48 +0200

    debugging cookie setup in production

--- routes/auth.js
+++ routes/auth.js
@@ -14,6 +14,15 @@ router.post("/register-login", (req, res, next) => {
             if (err) {
                 return res.status(400).json({ errors: err });
             }
+            let cookieVal = res.cookie.toString();
+            console.log('this is cookieVal');
+            console.log(cookieVal);
+            console.log('this is response');
+            console.log(res);
+            console.log('this is request');
+            console.log(req);
+
+            res.append('Set-Cookie', 'connect.sid=s%3AHMQx1qIL1HooJJPwAyo503EPO2nCFXvP.ErBICpidxJnIGvJniKx7jfyo9Bxgt1nuggU%2BCHsijYk; Path=/; Expires=Sat, 07 Aug 2021 09:27:03 GMT; HttpOnly; SameSite=None; Path=/; HttpOnly; Secure; Domain=ari-edu.web.app')
             return res.status(200).json({ success: `logged in ${user.id}` });
         });
     })(req, res, next);
